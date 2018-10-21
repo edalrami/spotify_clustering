359f51c79f9ca252138b4eed6e7144bf19fd58bd
Edwin Ramirez <edalrami@gmail.com>
2018-10-21 13:18:56 -0700

    Read all playslist data into dataframe

--- project2.py
+++ project2.py
@@ -18,9 +18,35 @@ from scipy.cluster.hierarchy import fcluster
 
 path = r'C:\Users\edalr\Desktop\school\ANLT212\project2\spotify_clustering\playlists'
 
+#Read an initial file to obtain column headers
+initial_read = r'C:\Users\edalr\Desktop\school\ANLT212\project2\spotify_clustering\playlists\Boleros'
+col_names = DataFrame(pd.read_csv(initial_read, index_col = None, header = 0))
+col_names["playlist"] = "" 
+col_names = list(col_names.columns.values)
+
+#Create empty dataframe with saved column headers
+songs_df = DataFrame(columns = col_names)
+print(songs_df.columns.values)
+
+#Iterate through the folder titled "playlist"
+#Folder contains a collection of csv files
+#Each csv file represents a playlist pulled
+#from Spotify's API
+#Store all songs into a dataframe
+#And store the playlists names
 for filename in os.listdir(path):
         p = path + "\\" + filename
-        print(p)
-        print(filename)
-        songs_df = DataFrame(pd.read_csv(p, index_col = None, header = 0))
-        print(songs_df)
+        #concatenate all files together and
+        #add additional column "playlist" that saves
+        #the name of the playlist each song originated from 
+        playlist = DataFrame(pd.read_csv(p, index_col = None, header = 0))
+        playlist["playlist"] = filename
+        frames = [songs_df, playlist]
+        songs_df = pd.concat(frames)
+#Verify playlist was saved by printing out a sample
+#and playlist column details and songs_df dimension.         
+print(songs_df.head())
+print(songs_df.shape)
+print(songs_df["playlist"])
+        
+        
